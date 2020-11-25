bc30b41ac8629eb938e478693880e110e4a3819a
Pirjo Mure <n5589@student.jamk.fi>
2020-11-25 08:37:05 +0200

    sql

--- SQL_Asiakkaat_2.sql
+++ SQL_Asiakkaat_2.sql
@@ -1,5 +1,20 @@
 ﻿USE Asiakaspalvelu;
-SELECT * FROM Asiakas_Taulu;
-SELECT * FROM Tuote_Taulu;
-SELECT * FROM Tilaus_Taulu;
-SELECT * FROM TilausRivi_Taulu;
+--SELECT * FROM Asiakas_Taulu;
+--SELECT * FROM Tuote_Taulu;
+--SELECT * FROM Tilaus_Taulu;
+--SELECT * FROM TilausRivi_Taulu;
+
+SELECT Tuote_Taulu.Hinta, Tuote_Taulu.Tuote, TilausRivi_Taulu.Maara
+FROM Tuote_Taulu, Asiakas_Taulu,TilausRivi_Taulu;
+
+SELECT Asiakas_Taulu.Etunimi,Tuote_Taulu.Tuote
+FROM Asiakas_Taulu,Tuote_Taulu;
+
+SELECT Tuote,Maara, COUNT(Maara) FROM Tuote_Taulu,TilausRivi_Taulu
+    GROUP BY Tuote, Maara;
+
+    SELECT SUM(Maara) FROM TilausRivi_Taulu;
+
+    SELECT Tuote,Hinta FROM Tuote_Taulu
+    GROUP BY Hinta,Tuote;
+
---
+++ dbo.Tuote_Taulu.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Tuote_Taulu] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Tuotenumero] INT           NULL,
+    [Tuote]       NVARCHAR (50) NULL,
+    [Hinta]       MONEY         NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_TuoteTaulu_ToTable] FOREIGN KEY ([Tuotenumero]) REFERENCES [Tuote_Taulu]([Id]), 
+
+);
+
